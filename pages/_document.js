c1aae3f47b612cbc26aa58d3fce3b0cf7b80afc9
Pancham Barman <panchamb63@gmail.com>
2023-06-02 10:55:52 +0530

    Improved the code

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,11 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <script
+          async
+          src="https://lynx-umami.vercel.app/script.js"
+          data-website-id="50f6c269-e9b0-4768-a874-67749c679709"
+        ></script>
         <link rel="shortcut icon" href="/logo.svg" type="image/x-icon" />
       </Head>
       <body>
